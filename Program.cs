2e0466b2c0d78410016ebbd8fcee424db8badf23
Pedro Moura <pedro.moura@smarthis.com.br>
2023-01-30 08:58:38 -0300

    Game main menu

--- Program.cs
+++ Program.cs
@@ -1,43 +1,73 @@
 ﻿using System;
-using System.Text.Json;
 using RestSharp;
 using VirtualPetPokemon;
+using VirtualPetPokemon.TerminalInterface;
 
 var client = new RestClient("https://pokeapi.co/api/v2/pokemon/");
 var request = new RestRequest("");
 var response = await client.GetAsync(request);
 
-AllPokemons? result = JsonSerializer.Deserialize<AllPokemons>(response.Content);
+//AllPokemons? result = JsonSerializer.Deserialize<AllPokemons>(response.Content);
 
+//Console.WriteLine(@$"============================== Virtual Pokemon Pet ==============================");
+
+//Console.WriteLine(@"Hello! Below we have a list of Pokemons. Next to each name we have a number that 
+//will represent each one.Type a number to choose your new friend!");
+
+//for (int index = 0; index < result.results.Length; index++)
+//{
+//    Console.WriteLine($"{index + 1}\t{result.results.ToList()[index].name}");
+//}
+
+//int indexPokemon = int.Parse(Console.ReadLine());
+//MyPokemon pokemon = new MyPokemon();
+//pokemon.name = result.results.ToList()[indexPokemon - 1].name;
+
+
+//Console.Clear();
+//Console.WriteLine(@$"============================== Virtual Pokemon Pet ==============================");
+//Console.WriteLine(@$"Nice!! You choose {pokemon.name}, a very strong Pokemon. It's abilites are:");
+
+
+
+//request = new RestRequest($"{indexPokemon}/");
+//response = await client.GetAsync(request);
+//PokemonStats? pickPokemoninfo = JsonSerializer.Deserialize<PokemonStats>(response.Content);
+
+
+//foreach (var item in pickPokemoninfo.abilities)
+//{
+//    item.ShowAbility();
+//}
+bool flag = true;
+Menu gameMenu = new Menu();
 Console.WriteLine(@$"============================== Virtual Pokemon Pet ==============================");
 
-Console.WriteLine(@"Hello! Below we have a list of Pokemons. Next to each name we have a number that 
-will represent each one.Type a number to choose your new friend!");
-
-for (int index = 0; index < result.results.Length; index++)
+Console.WriteLine($@"
+1 - Adoption of a Pokemon
+2 - My Pokemons
+3 - Quit");
+while (flag)
 {
-    Console.WriteLine($"{index + 1}\t{result.results.ToList()[index].name}");
-}
+    string option = Console.ReadLine();
 
-int indexPokemon = int.Parse(Console.ReadLine());
-MyPokemon pokemon = new MyPokemon();
-pokemon.name = result.results.ToList()[indexPokemon - 1].name;
+    switch (option)
+    {
+        case "1":
+            int index = await gameMenu.ChoosePokemonAsync();
+            Console.WriteLine($"Index escolhido {index}");
+            break;
+        case "2":
+            gameMenu.MyPokemons();
+            break;
+        case "3":
+            Console.WriteLine("Closing...");
+            Thread.Sleep(1000);
+            flag = gameMenu.CloseGame();
+            break;
+    }
 
-
-Console.Clear();
-Console.WriteLine(@$"============================== Virtual Pokemon Pet ==============================");
-Console.WriteLine(@$"Nice!! You choose {pokemon.name}, a very strong Pokemon. It's abilites are:");
-
-
-
-request = new RestRequest($"{indexPokemon}/");
-response = await client.GetAsync(request);
-PokemonStats? pickPokemoninfo = JsonSerializer.Deserialize<PokemonStats>(response.Content);
-
-
-foreach (var item in pickPokemoninfo.abilities)
-{
-    item.ShowAbility();
+    //Console.ReadKey();
 }
 
 
